d3e2f40007931231a672e331c9c224e5fd793c59
tesliker <tesliker@sliker.com>
2018-05-08 10:56:59 -0400

    POC - still having trouble parsing CLI return data

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,20 +2,47 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+var cmd = require('node-cmd');
 
 type Props = {};
 
 export default class Home extends Component<Props> {
-  props: Props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+    };
+    this.listProjects= this.listProjects.bind(this);
+  }
 
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <button onClick={this.listProjects}>List projects</button>
         </div>
       </div>
     );
+  }
+
+  listProjects() {
+    cmd.get(
+        'fin project list',
+        function(err, data, stderr){
+          var stringObj = data.split(/ {2,}/g);
+          var tableSize = 4;
+          var tableObj = {};
+          for (var i = 0; i < tableSize; i++) {
+            var column = [];
+            for (var n = i; n < stringObj.length; n+=tableSize - 1) {
+              column.push(stringObj[n]);
+            }
+            var key = stringObj[i];
+            tableObj[key] = column;
+          }
+          console.log(tableObj);
+        }
+    );
+
   }
 }
